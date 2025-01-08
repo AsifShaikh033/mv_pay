29bfd49eafe497234b0e452766b2f93c26fc219d
MonikaPanchal06 <monikapanchal1402@gmail.com>
2025-01-08 13:44:03 +0530

    transaction table

--- resources/views/Admin/layout/sidebar.blade.php
+++ resources/views/Admin/layout/sidebar.blade.php
@@ -4,12 +4,13 @@
           <div class="logo-header" data-background-color="dark">
             <a href="index.html" class="logo">
               <img
-                 src="{{ asset('assets/img/mvpay/logo_light.svg') }}"
+                src="{{ asset('storage/' . \App\Helpers\Helper::webConfig('logo')) }}"
                 alt="navbar brand"
                 class="navbar-brand"
                 height="20"
               />
             </a>
+           
             <div class="nav-toggle">
               <button class="btn btn-toggle toggle-sidebar">
                 <i class="gg-menu-right"></i>
