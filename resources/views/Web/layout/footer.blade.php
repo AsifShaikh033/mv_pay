f0c97181f2aa916f66f38efcdf0137f02161c008
asifshaikh@oyelabs.com <monikapanchal1402@gmail.com>
2025-01-20 13:07:22 +0530

    home page redirect

--- resources/views/Web/layout/footer.blade.php
+++ resources/views/Web/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="div_footer_x">
     <div class="div_footer_x_overlay">
         <ul class="footer_icons">
-            <li><a href="Services-2.html" title="home" class="cls_f_home"><i class="flaticon-381-home"></i></a></li>
+            <li><a href="{{route('index')}}" title="home" class="cls_f_home"><i class="flaticon-381-home"></i></a></li>
             <li><a class="cls_search_searvices"><i class="flaticon-381-search-1"></i></a></li>
             
             <li><a onclick="return fn_checkLogin()" title="History"><i class="flaticon-381-list-1"></i></a></li>
