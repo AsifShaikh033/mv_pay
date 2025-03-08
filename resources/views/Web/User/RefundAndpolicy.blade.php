f51d051d9994e8a81fa0ed7fe4b952dce2c0a260
priyanaka-patel2001 <priyanka.patel@oyelabs.com>
2025-03-08 16:08:06 +0530

    design done

--- resources/views/Web/User/RefundAndpolicy.blade.php
+++ resources/views/Web/User/RefundAndpolicy.blade.php
@@ -12,15 +12,15 @@
           <div>
             <h3 class="text-light"> Cancellation and Refund Policy</h3>
           </div>
-          <p> We have pledged to provide you world class products and services for every single business you have made with us in near past, or future and as of now. We want you to have a swift and hassle free experience while you access the Nidhi CSP</p>
-          <p> We certify you about the quality and genuineness of the entire product and service range on the Nidhi CSP web portal. But in any case, you have any lack of contentment and grievance toward our product or services, we apologize for that, and you will be served with our Peace of Mind policy.</p>
+          <p> We have pledged to provide you world class products and services for every single business you have made with us in near past, or future and as of now. We want you to have a swift and hassle free experience while you access the MV EASY PAY</p>
+          <p> We certify you about the quality and genuineness of the entire product and service range on the MV EASY PAY web portal. But in any case, you have any lack of contentment and grievance toward our product or services, we apologize for that, and you will be served with our Peace of Mind policy.</p>
           <p>
             <strong class="text-white">Unused Balance Clause</strong>
           </p>
           <p> Any top-up payment / advance payment which is received from retailers / distributors deposit the advance payment to his retail account for, the retailer / distributor does not use the amount for goods &amp; services and he wants his payment to return in this case after one month of deposit, retailer need to give refund request to us, once we will get refund request from the retailer, the balance amount will be refunded within 15 to 20 working days via same source.</p>
           <h3 class="text-light"> RETURN AND EXCHANGE</h3>
           <p> Our entire Product and Service catalogue is backed up by 3 day return and exchange policy, in any case of error, or mishap happen during placement of the product, from the first of product or service acquisition to the complaint registration date.</p>
-          <p>Products or services, like Bus Tickets, Flight Tickets, Rail Tickets, DTH Recharge, Hotel Booking are only liable for return and exchange only when purchased from MV PAY (mvpay.mvvision.in ) web portal. We do not accept third-party products under our policy.</p>
+          <p>Products or services, like Bus Tickets, Flight Tickets, Rail Tickets, DTH Recharge, Hotel Booking are only liable for return and exchange only when purchased from MV EASY PAY (mvpay.mvvision.in ) web portal. We do not accept third-party products under our policy.</p>
           <p> In any case, if someone has not received their product or service, but have made the transaction, can reacquisition of reacquisition of product or avail another service of same monetary value, simply by making a phone call.</p>
           <p> A product or service is only liable under return and exchange policy if the request is made with 3 days of its acquisition, and accepted channels for making such request are our official support mail address and second by contacting through our helpline number.</p>
           <h3 class="text-light"> CANCELLATION</h3>
@@ -39,7 +39,7 @@
           
           <p> We only can refund the partial or full amount of transaction, accord to company policy on product and service, for more information, you need to specifically go through their cancellation policy rules.</p>
 
-          <p> Note : All sales of Recharge are final and there will be no refund or exchange permitted. Please be advised that You are responsible for the mobile number or DTH account number you purchase Recharge for and all charges that result from those purchases. Nidhi CSP is not responsible for any purchase of Recharge for an incorrect mobile number or DTH account number.</p>
+          <p> Note : All sales of Recharge are final and there will be no refund or exchange permitted. Please be advised that You are responsible for the mobile number or DTH account number you purchase Recharge for and all charges that result from those purchases. MV EASY PAY is not responsible for any purchase of Recharge for an incorrect mobile number or DTH account number.</p>
 
           <p>Note : However, in a case where a transaction has been completed by you on the Site, and money has been charged to your card or bank account but a Recharge has not delivered within 24 hours of your completion of the transaction then you may inform us by sending us an email on  &nbsp; <a href="mailto:">Mail</a>&nbsp; or posting us a message on the  &nbsp; <strong class="text-white">Help</strong>&nbsp; option available in your panel. In such a scenario you will be entitled to a full refund. We request you to include in the email the following details - the mobile number or DTH account number, operator name, Recharge value, Transaction date and Order Number Company shall investigate the incident and if it is found that money was indeed charged to your card or bank account without delivery of the Recharge then you will be refunded the money within 7 working days from the date of the receipt of your email.</p><p> &nbsp;</p>
         </div>
