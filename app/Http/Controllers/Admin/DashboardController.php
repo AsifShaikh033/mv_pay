866a03855b5b61c295870a0ab5f8cf0eab15c455
your-username <asif.shaikh@oyelabs.com>
2025-03-15 15:13:07 +0530

    admin home

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use App\Models\LeadGeneration;
 use App\Models\User;
 
 class DashboardController extends Controller
@@ -12,8 +13,9 @@ class DashboardController extends Controller
     public function index(){
         $latestUsers = User::latest()->take(10)->get();
         $user_count = User::all()->count();
+        $LeadGeneration = LeadGeneration::all()->count();
 
         $latestTransactions = Transaction::latest()->take(10)->with('user')->get();
-        return view('Admin.index', compact('latestUsers', 'latestTransactions','user_count'));
+        return view('Admin.index', compact('latestUsers', 'latestTransactions','user_count','LeadGeneration'));
     }
 }
